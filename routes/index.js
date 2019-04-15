6266842abb41eeac44bf52cec05a9c571fd96b83
user-penguin <kobzev96@gmail.com>
2019-04-15 21:22:01 +0700

    add authors pug page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET author page. */
+router.get('/author', function(req, res, next) {
+  res.render('author');
+});
+
+/* GET Authors */
+router.get('/authors', function (req, res, next) {
+  res.render('authors');
+});
+
+
 module.exports = router;
