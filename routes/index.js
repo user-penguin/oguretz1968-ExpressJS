8713bf0e451b332106881f69e960a8996d0aa5f2
user-penguin <kobzev96@gmail.com>
2019-04-18 16:31:26 +0700

    add getting article with authors in route

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,39 @@
-var express = require('express');
-var router = express.Router();
-const Author = require('../modules/database/models/Author');
+var express = require('express')
+var router = express.Router()
+const Author = require('../modules/database/models/Author')
+const Article = require('../modules/database/models/Article')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+router.get('/', function (req, res, next) {
+  res.render('index')
+})
 
 /* GET Authors */
 router.get('/authors', function (req, res, next) {
-  res.render('authors');
-});
+  res.render('authors')
+})
 
 /* GET AuthorAdmins page */
 router.get('/authorAdmin', async function (req, res, next) {
   let authorData = await Author.findAll({
 
-  }).catch((err)=>{
-    console.log(err);
-  });
-  res.render('authorAdmin', {authors: authorData});
-});
+  }).catch((err) => {
+    console.log(err)
+  })
+  res.render('authorAdmin', { authors: authorData })
+})
 
+/* GET Article admin page */
+router.get('/articleAdmin', async function (req, res, next) {
+  let articleWithAuthors = await Article.findAll({
+    include: [{ author: Author }]
+  })
+    .catch((err) => {
+      console.log(err)
+    })
+  res.render('articleWithAuthor', {
+    articleWithAuthor: articleWithAuthors
+  })
+})
 
-module.exports = router;
+module.exports = router
